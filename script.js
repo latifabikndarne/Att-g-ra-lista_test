6a9e8eef57095b7b055412467fed0fb0f1d5d13c
latifabikndarne <latifa.bikndarne@gmail.com>
2025-10-06 12:00:10 +0200

    två html filer added

--- script.js
+++ script.js
@@ -1,52 +1,76 @@
-// Variabler deklaration
+// Att deklarera en variabel
 const completedElement = document.querySelector("#completed");
 const inputTodo = document.querySelector("#inputTodo");
 const addTodoButton = document.querySelector("#addTodoButton");
 const infoTextElement = document.querySelector("small");
 const todolist = document.querySelector("#todolist");
 
-let todoText = "";
+
 let completed = 0;
-    // Eventlistener för knappen
+let todoText = "";
+let text = inputTodo.value;
+infoTextElement.textContent= " ";
+
+
 addTodoButton.addEventListener("click", addToDo);
-
-
-
-// Funktion för att lägga till todo
-
 function addToDo() {
-    infoTextElement.textContent = "";
+
     todoText = inputTodo.value;
 
-    if (todoText.length == 0) {
+    if (todoText == 0) {
         infoTextElement.textContent = "Skriv en uppgift!";
         return;
     }
-    
-    //Att deklarera Li element och sedan addera li element i ul listan
     const item = document.createElement("li");
     todolist.appendChild(item);
-    // span element för att kunna markera som klar endast texten.
-    const itemText = document.createElement("span");
-    itemText.innerText = todoText;
-    // Eventlistener på varje li element för att kunna markera som klar.
-    itemText.addEventListener("click", 
-     function() {
-        if (item.getAttribute("class") == "completed") {
-        item.setAttribute("class", "");
-        completed--;
-         completedElement.textContent = "Utförda uppgifter: " + completed;
-        
-        }else {
-            item.setAttribute("class", "completed");
-            completed++;
-            completedElement.textContent = "Utförda uppgifter: " + completed;
-        }
+   
 
+//span element för att behålla texten
+const itemText = document.createElement("span");
+itemText.innerText = todoText;
+
+//EventListener för varje item
+
+itemText.addEventListener("click",
+    function () {
+
+        if (itemText.getAttribute("class") == "completed") {
+            itemText.setAttribute("class", "");
+            completed--;
+            completedElement.innerText = "Utförda uppgifter: " + completed;
+
+        } else {
+            itemText.setAttribute("class", "completed");
+            completed++;
+            completedElement.innerText = "Utförda uppgifter: " + completed;
+        }
+      
     });
-    // för att få in texten i li elementet
-    item.appendChild(itemText);
+ 
+      item.appendChild(itemText);
     
-    // För att kunna tomma inputfältet.
+
+// att lägga till papperskorgsikon
+    const trashcan = document.createElement("span");
+    trashcan.innerHTML = "&#128465"; //papperskorgsikon
+    trashcan.setAttribute("class", "trashcan");
+    item.appendChild(trashcan);
+
+//EventListener för papperskorgsikon
+    trashcan.addEventListener("click",
+    function () {
+        todolist.removeChild(item);
+
+       }); 
+    
+  
     inputTodo.value = "";
-}
+} 
+    
+
+
+       
+
+
+
+    
